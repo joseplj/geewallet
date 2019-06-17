aeaf163eb962adbdccbdd2adde7e208fb17ed76d
Andres G. Aragoneses <knocte@gmail.com>
2019-06-17 18:41:11 +0700

    try new nuget & give more info

--- scripts/install_mono_from_microsoft_deb_packages.sh
+++ scripts/install_mono_from_microsoft_deb_packages.sh
@@ -11,4 +11,8 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 apt update
 apt install -y mono-devel fsharp
+
 mono --version
+msbuild -version
+
+find /usr | grep NuGet | grep Packaging
