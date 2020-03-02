2d4d2dbfa029093b8763f97ee36be107987e0e42
Andres G. Aragoneses <knocte@gmail.com>
2020-03-02 21:45:23 +0800

    Require at least Mono 6.6, to get rid of ugly workarounds

--- src/GWallet.Backend/Ether/EtherServer.fs
+++ src/GWallet.Backend/Ether/EtherServer.fs
@@ -114,11 +114,6 @@ module Server =
             if webEx.Status = WebExceptionStatus.TrustFailure then
                 raise <| ServerChannelNegotiationException(exMsg, webEx.Status, webEx)
 
-            // as Ubuntu 18.04's Mono (4.6.2) doesn't have TLS1.2 support, this below is more likely to happen:
-            if not Networking.Tls12Support then
-                if webEx.Status = WebExceptionStatus.SendFailure then
-                    raise <| ServerUnreachableException(exMsg, webEx)
-
             raise <| UnhandledWebException(webEx.Status, webEx)
 
         | None ->
@@ -306,9 +301,6 @@ module Server =
 
     let private FaultTolerantParallelClientDefaultSettings (mode: ServerSelectionMode) =
         let numberOfConsistentResponsesRequired =
-            if not Networking.Tls12Support then
-                1u
-            else
                 2u
         FaultTolerantParallelClientInnerSettings numberOfConsistentResponsesRequired
                                                  mode
--- src/GWallet.Backend/UtxoCoin/UtxoCoinServer.fs
+++ src/GWallet.Backend/UtxoCoin/UtxoCoinServer.fs
@@ -37,7 +37,7 @@ module Server =
                     {
                         ServerSelectionMode = mode
                         ConsistencyConfig = consistencyConfig
-                        ReportUncanceledJobs = (not Config.LegacyUtxoTcpClientEnabled)
+                        ReportUncanceledJobs = true
                     }
         }
 
